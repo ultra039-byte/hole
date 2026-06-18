fe552561998001d5308665c4cbaccf1a5a007a36
ultra039-byte <ultra039@gmail.com>
2026-06-18 21:41:45 +0300

    Update config.py

--- config.py
+++ config.py
@@ -1,1 +1,12 @@
-# Конфигурация бота
+import pytz
+
+# Жёстко прописываем токены прямо в код
+MAX_BOT_TOKEN = "f9LHodD0cOKcINRwAI3a7WF4XKt0KCzAXvoyYwjUPzLNr-r2bxu9685aqelAGFiA27iz_zMRw54hmI6WI9px"
+VPOSTER_API_TOKEN = "vp1.a.4atLJdJ0TVWY0yrT14_4OWmNgiIi4ZRXGk86KqcMtYdMDUK4KmaFwGUpZO2QOn_XSyCaxBfRpPkRRXhWw2UBM"
+
+# API URL-адреса платформ
+MAX_API_URL = "https://api.max-messenger.ru/v1" 
+VPOSTER_API_URL = "https://api.vposter.ru/v1"
+
+# Наша рабочая таймзона
+MOSCOW_TZ = pytz.timezone('Europe/Moscow')
