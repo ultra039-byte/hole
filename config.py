3face6dc7f956498e47b6734961df328a8455d1e
ultra039-byte <ultra039@gmail.com>
2026-06-18 22:36:59 +0300

    Update config.py

--- config.py
+++ config.py
@@ -4,7 +4,7 @@ import pytz
 MAX_BOT_TOKEN = os.getenv("MAX_BOT_TOKEN") or os.getenv("BOT_TOKEN") or os.getenv("MAX_TOKEN")
 VPOSTER_API_TOKEN = os.getenv("VPOSTER_API_TOKEN")
 
-MAX_API_URL = "https://api.max-messenger.ru/v1"
+MAX_API_URL = "https://platform-api.max.ru"
 VPOSTER_API_URL = "https://api.vposter.ru/v1"
 
 MOSCOW_TZ = pytz.timezone('Europe/Moscow')
